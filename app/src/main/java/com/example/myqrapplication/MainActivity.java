daac81b876a1a24619d146479d3cc85dd3d231dd
jingjing yang <jingdingdu@gmail.com>
2023-10-05 11:35:47 +0300

    add QR Code scanning functionality on button click

--- app/src/main/java/com/example/myqrapplication/MainActivity.java
+++ app/src/main/java/com/example/myqrapplication/MainActivity.java
@@ -1,12 +1,49 @@
 package com.example.myqrapplication;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.Manifest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_PERMISSION_REQUEST = 1;
+    private Button scanButton;
+
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanButton = findViewById(R.id.scanButton);
+
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hasCameraPermission()) {
+                    // Proceed with QR code scanning
+                    // Create a new Intent to launch the QR Code scanning activity
+                    Intent intent = new Intent(MainActivity.this, QrCodeScannerActivity.class);
+                    startActivity(intent);
+                } else {
+                    requestCameraPermission();
+                }
+
+            }
+        });
     }
 }
