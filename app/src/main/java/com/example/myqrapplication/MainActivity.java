487fdc61d22df248a6df5273e7bc4302a1e5b124
jingjing yang <jingdingdu@gmail.com>
2023-10-10 12:05:39 +0300

    Add distance calculation

--- app/src/main/java/com/example/myqrapplication/MainActivity.java
+++ app/src/main/java/com/example/myqrapplication/MainActivity.java
@@ -1,49 +1,177 @@
 package com.example.myqrapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.Manifest;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MainActivity extends AppCompatActivity {
-    private static final int CAMERA_PERMISSION_REQUEST = 1;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 42;
+    private static final int QR_CODE_REQUEST_CODE = 83;
+
+    private TextView userLocationLabel;
     private Button scanButton;
+    private TextView scannedLocationLabel;
+    private TextView distanceLabel;
 
-    private boolean hasCameraPermission() {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
-    }
+    Location userLocation;
 
-    private void requestCameraPermission() {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST);
-    }
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        userLocationLabel = findViewById(R.id.user_lat_long);
+
+        scannedLocationLabel = findViewById(R.id.scanned_lat_long);
+
+        distanceLabel = findViewById(R.id.distance);
+
+        // Initialize the scanButton
         scanButton = findViewById(R.id.scanButton);
+        scanButton.setOnClickListener(this);
 
+        // Initialize the fusedLocationClient variable with an instance of the FusedLocationProviderClient class,
+        // which provides access to the device's last known location
+        // as well as the ability to request periodic location updates.
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
 
-        scanButton.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        // Start the ScanActivity
+        Intent intent = new Intent(this, ScanActivity.class);
+        startActivityForResult(intent, QR_CODE_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if location permissions are granted
+        if (checkLocationPermissions()) {
+            // Get the last known location
+            getLastKnownLocation();
+        }
+    }
+
+    private boolean checkLocationPermissions() {
+        // Check if location permissions are granted
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Request location permissions
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            return false;
+        }
+        return true;
+    }
+
+    private void getLastKnownLocation() {
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
             @Override
-            public void onClick(View v) {
-                if (hasCameraPermission()) {
-                    // Proceed with QR code scanning
-                    // Create a new Intent to launch the QR Code scanning activity
-                    Intent intent = new Intent(MainActivity.this, QrCodeScannerActivity.class);
-                    startActivity(intent);
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    // Display latitude and longitude if location is available
+                    userLocationLabel.setText("Current Lat: " + location.getLatitude() + "\n\nCurrent Long: " + location.getLongitude());
+                    // Get the user location
+                    userLocation = location;
                 } else {
-                    requestCameraPermission();
+                    // Display "Location not available" if location is null
+                    userLocationLabel.setText("Location not available");
                 }
-
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (checkLocationPermissions()) {
+                // Get the last known location
+                getLastKnownLocation();
+            } else {
+                // Display "Permission denied" if location permissions are not granted
+                userLocationLabel.setText("Permission denied");
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Check if the result is from the QR code scanner activity
+        if (requestCode == QR_CODE_REQUEST_CODE) {
+            // Check if the result is successful
+            if (resultCode == RESULT_OK) {
+                // Get the scanned QR code contents from the intent data
+                String qrCodeContents = data.getStringExtra("qrCodeContents");
+
+                calculateDistance(qrCodeContents, userLocation);
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle the case when the QR code scanning is canceled
+                // Update the the user interface
+                scannedLocationLabel.setText("");
+                distanceLabel.setText("");
+            }
+        }
+    }
+
+    // The function takes two parameters: qrCodeContents, which represents the contents of the QR code,
+    // and userLocation, which represents the user's current location.
+    private void calculateDistance(String qrCodeContents, Location userLocation) {
+        // Parse the geo: URI from the QR code contents and extract the latitude and longitude
+        String geoUri = qrCodeContents.split(":")[1]; // Remove the "geo:" prefix
+        String[] coordinates = geoUri.split(",");
+
+        if (coordinates.length != 2) {
+            // Invalid QR code contents, handle the error or show a message to the user
+            scannedLocationLabel.setText("Invalid latitude or longitude values! Two Coordinates please!");
+            distanceLabel.setText("");
+        } else {
+            try {
+
+                double qrCodeLatitude = Double.parseDouble(coordinates[0]);
+                double qrCodeLongitude = Double.parseDouble(coordinates[1]);
+
+                // Ensure that the latitude is between -90 and 90, and the longitude is between -180 and 180.
+                if (qrCodeLatitude < -90 || qrCodeLatitude > 90 || qrCodeLongitude < -180 || qrCodeLongitude > 180) {
+                    // Invalid latitude or longitude values, handle the error or show a message to the user
+                    scannedLocationLabel.setText("Invalid latitude or longitude values! Out of range error!");
+                    distanceLabel.setText("");
+                } else {
+                    scannedLocationLabel.setText("Decoded Lat: " + String.valueOf(qrCodeLatitude) + "\n\nDecoded Long: " + String.valueOf(qrCodeLongitude));
+
+                    // Calculate the distance between the QR code location and the user's location using the Location.distanceBetween() method
+                    float[] results = new float[1];
+                    Location.distanceBetween(userLocation.getLatitude(), userLocation.getLongitude(), qrCodeLatitude, qrCodeLongitude, results);
+                    float distanceInMeters = results[0];
+
+                    // Convert the distance from meters to kilometers
+                    double distanceInKilometers = distanceInMeters / 1000.0;
+
+                    // Update the distance label in the user interface
+                    distanceLabel.setText("Distance: " + distanceInKilometers + " km");
+                }
+            } catch (NumberFormatException e) {
+                // Invalid latitude or longitude values, handle the error or show a message to the user
+                scannedLocationLabel.setText("Invalid latitude or longitude values! Need Numeric Coordinates!");
+                distanceLabel.setText("");
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/myqrapplication/ScanActivity.java
@@ -0,0 +1,165 @@
+package com.example.myqrapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.Manifest;
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class ScanActivity extends AppCompatActivity {
+    SurfaceView surfaceViewLabel;
+    TextView barcodeValueLabel;
+    Button finishButton;
+
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 201;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        surfaceViewLabel = findViewById(R.id.surfaceView);
+        barcodeValueLabel = findViewById(R.id.txtBarcodeValue);
+        finishButton = findViewById(R.id.finishButton);
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle the "Finish" button click event
+                // Finish the activity
+                finish();
+            }
+        });
+
+        // Initialize barcode detectors and camera sources
+        initialiseDetectorsAndSources();
+    }
+
+    private void initialiseDetectorsAndSources() {
+        Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
+
+        // Create a barcode detector with QR_CODE format
+        barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
+
+        // Create a camera source with autofocus and preferred preview size
+        cameraSource = new CameraSource.Builder(this, barcodeDetector).setRequestedPreviewSize(1920, 1080).setAutoFocusEnabled(true).build();
+
+        // Set up the SurfaceView for the camera preview
+        surfaceViewLabel.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    // Check camera permission and start the camera preview
+                    if (ActivityCompat.checkSelfPermission(ScanActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(surfaceViewLabel.getHolder());
+                    } else {
+                        // Request camera permission if not granted
+                        ActivityCompat.requestPermissions(ScanActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                // Not used in this example
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                // Stop the camera when the SurfaceView is destroyed
+                cameraSource.stop();
+            }
+        });
+
+        // Set up the barcode detector processor
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                //Release resources to prevent memory leaks
+                Toast.makeText(getApplicationContext(),
+                        "To prevent memory leaks barcode scanner has been stopped",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                // Process detected barcodes and update UI on the main thread
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() != 0) {
+                    barcodeValueLabel.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            String contents = barcodes.valueAt(0).displayValue;
+                            // Update the UI with the barcode value
+                            barcodeValueLabel.setText("Scanned Contents:\n " + contents);
+
+                            // Create a new intent to pass the scanned QR code contents back to the MainActivity
+                            Intent resultIntent = new Intent();
+                            resultIntent.putExtra("qrCodeContents", contents);
+
+                            // Set the result as OK and pass the result intent back to the MainActivity
+                            setResult(RESULT_OK, resultIntent);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Release camera resources when the activity is paused
+        if (cameraSource != null) {
+            cameraSource.release();
+        }
+    }
+
+    // Handle the result of the camera permission request
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Camera permission granted, start the camera preview
+                try {
+                    cameraSource.start(surfaceViewLabel.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                // Camera permission denied, display a message or take appropriate action
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Release barcode detector resources when the activity is destroyed
+        if (barcodeDetector != null) {
+            barcodeDetector.release();
+        }
+    }
+}
